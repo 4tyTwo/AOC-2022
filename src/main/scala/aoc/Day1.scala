2e3e0f4721d203cd51f5ce1c5c01f2fa742228a2
Igor Toporkov <iigortoporkov@gmail.com>
2022-12-01 20:34:46 +0100

    Alternative, more efficient Day1 solution

--- src/main/scala/aoc/Day1.scala
+++ src/main/scala/aoc/Day1.scala
@@ -6,7 +6,7 @@ case object Day1 extends AOCProblem {
 
   private val input = scala.io.Source.fromFile("./priv/day1/input.txt").getLines().toSeq
 
-  val caloriesPerElf: Seq[Int] =
+  def caloriesPerElf: Seq[Int] =
     input.mkString("\n")
       .split("\n\n")
       .map(_.split("\n").toSeq.map(_.toInt))
@@ -18,5 +18,33 @@ case object Day1 extends AOCProblem {
 
   def solve2: Int = {
     caloriesPerElf.sorted.reverse.take(3).sum
+  }
+}
+
+case object Day1Alt extends AOCProblem {
+  // More efficient, less scala-like
+  val day = 1
+  private val input = scala.io.Source.fromFile("./priv/day1/input.txt").getLines().toSeq
+
+  def createCaloriesPQ: collection.mutable.PriorityQueue[Int] = {
+    val pq = collection.mutable.PriorityQueue.empty[Int]
+    var acc = 0
+    input.foreach( s =>
+      if (s.isBlank) {
+        pq.addOne(acc)
+        acc = 0
+      } else {
+        acc += s.toInt
+      }
+    )
+    pq
+  }
+
+  def solve1: Int = {
+    createCaloriesPQ.dequeue()
+  }
+
+  def solve2: Int = {
+    createCaloriesPQ.dequeueAll.take(3).sum
   }
 }
--- src/main/scala/aoc/Main.scala
+++ src/main/scala/aoc/Main.scala
@@ -3,6 +3,7 @@ package aoc
 object Main extends App {
 
   val problems: Seq[AOCProblem] = Seq(
+    Day1
   )
   problems.foreach(_.printAnswers)
 }
