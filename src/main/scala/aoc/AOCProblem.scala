fb92a0d573b4402dd65af7b9d002243887c71ee4
Igor Toporkov <iigortoporkov@gmail.com>
2022-12-01 20:14:38 +0100

    Log execution time

--- src/main/scala/aoc/AOCProblem.scala
+++ src/main/scala/aoc/AOCProblem.scala
@@ -8,7 +8,17 @@ trait AOCProblem {
   def solve2: Int
 
   def printAnswers: Unit = {
-    println(s"Day $day part 1: $solve1")
-    println(s"Day $day part 2: $solve2")
+    val (t1, r1) = time(solve1)
+    val (t2, r2) = time(solve2)
+    println(s"Day $day part 1: $r1, Elapsed time: $t1 μs")
+    println(s"Day $day part 2: $r2, Elapsed time: $t2 μs")
+  }
+
+  // time in μs
+  private def time[R](block: => R): (Long, R) = {
+    val t0 = System.nanoTime()
+    val result = block
+    val t1 = System.nanoTime()
+    ((t1 - t0) / 1000, result)
   }
 }
